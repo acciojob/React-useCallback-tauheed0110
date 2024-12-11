bb1b09d92824f326ac0732f745b03a1ddc0006cf
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-11 16:34:30 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,47 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useCallback, useState } from 'react';
 
+const App = () => {
+    const [input, setInput] = useState('');
+    const [skills, setSkills] = useState([]);
+
+    function handleClick(e){
+        e.preventDefault();
+        setSkills([...skills, input.trim()]);
+        setInput('');
+    }
+    const handleSkill = useCallback((value)=>{
+        setSkills(skills.filter(skill => skill !== value));
+    }, [skills]);
+    
+  return (
+    <div>
+        <h1 id='heading'>Add Skills To The List</h1>
+        <form>
+            <input id='skill-input' type='text' value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+            <button id='skill-add-btn' onClick={(e)=>{handleClick(e)}}>Add Skill</button>
+        </form>
+        <UseCallbackComp skills={skills} handleSkill={handleSkill}/>
+    </div>
+  );
+}
+
+export default App;
+
+
+function UseCallbackComp({skills, handleSkill}){
+
+    return (
+        <div>
+            <ul>
+                {
+                    skills.map((skill, index) => {
+                        return <li id={`skill-number-${index}`} key={index}>
+                                    {skill}
+                                    <button onClick={()=>{handleSkill(skill)}}>Delete</button>
+                                </li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
